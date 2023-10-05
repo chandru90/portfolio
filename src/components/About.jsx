06e3fe8a21a3c6a6313ae237f45110962b9e1e1b
chandru90 <iochandru90@gmail.com>
2023-10-05 06:12:07 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import aboutSvg from '../assets/about.svg';
+import SectionTitle from './SectionTitle';
+const About = () => {
+  return (
+    <section className='bg-white py-20' id='about'>
+      <div className='align-element grid md:grid-cols-2 items-center gap-16'>
+        <img src={aboutSvg} className='w-full h-64' />
+        <article>
+          <SectionTitle/>
+          
+        </article>
+      </div>
+    </section>
+  );
+};
+export default About;
